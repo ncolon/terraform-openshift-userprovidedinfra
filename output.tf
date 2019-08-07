193b562eeb7d2a595f870090354ada6f5acd5de9
Noel Colon <ncolon@us.ibm.com>
2019-08-07 12:28:11 -0400

    first commit

---
+++ output.tf
@@ -0,0 +1,63 @@
+
+output "bastion_hostname" {
+    value = "${var.bastion_hostname}"
+}
+
+output "bastion_public_ip" {
+    value = "${var.bastion_public_ip}"
+}
+
+output "haproxy_hostname" {
+    value = "${var.haproxy_hostname}"
+}
+output "haproxy_public_ip" {
+    value = "${var.haproxy_public_ip}"
+}
+
+output "infra_hostname" {
+    value = "${var.infra_hostname}"
+}
+
+output "infra_private_ip" {
+    value = "${var.infra_private_ip}"
+}
+
+output "master_hostname" {
+    value = "${var.master_hostname}"
+}
+
+output "master_private_ip" {
+    value = "${var.master_private_ip}"
+}
+
+output "password" {
+    value = "${var.password}"
+}
+
+output "storage_hostname" {
+    value = "${var.storage_hostname}"
+}
+
+output "storage_private_ip" {
+    value = "${var.storage_private_ip}"
+}
+
+output "username" {
+    value = "${var.username}"
+}
+
+output "worker_hostname" {
+    value = "${var.worker_hostname}"
+}
+
+output "worker_private_ip" {
+    value = "${var.worker_private_ip}"
+}
+
+output "public_master_vip" {
+    value = "${var.public_master_vip}"
+}
+
+output "public_app_vip" {
+    value = "${var.public_app_vip}"
+}
---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "bastion_hostname" {}
+variable "bastion_public_ip" {}
+variable "master_hostname"   { type = "list" }
+variable "master_private_ip" { type = "list" }
+variable "infra_hostname"    { type = "list" }
+variable "infra_private_ip"  { type = "list" }
+variable "worker_hostname"   { type = "list" }
+variable "worker_private_ip" { type = "list" }
+variable "storage_hostname"  {
+    type = "list"
+    default = []
+}
+variable "storage_private_ip" {
+    type = "list"
+    default = []
+}
+variable "haproxy_hostname"  {
+    type = "list"
+    default = []
+}
+variable "haproxy_public_ip" {
+    type = "list"
+    default = []
+}
+
+variable "password" {}
+variable "username" {}
+
+
+variable "public_master_vip" {}
+
+variable "public_app_vip" {}
